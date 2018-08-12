568a107f267b0fa8202b268427ff6f9cf13ce072
Musa Musayev <musa@musayev.io>
2018-08-12 10:12:25 -0400

    Updated Root module to deploy Networking resources

--- main.tf
+++ main.tf
@@ -2,8 +2,16 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-# Deploy Storage Resource
+# Deploy Storage Resources
 module "storage" {
   source       = "./storage"
   project_name = "${var.project_name}"
+}
+
+# Deploy Networking Resources
+module "networking" {
+  source = "./networking"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip = "${var.accessip}"
 }
--- networking/variables.tf
+++ networking/variables.tf
@@ -1,16 +1,9 @@
 # -----------networking/variables.tf-----------
 
-variable "vpc_cidr" {
-  default = "10.255.0.0/16"
-}
+variable "vpc_cidr" {}
 
 variable "public_cidrs" {
-  default = [
-    "10.255.1.0/24",
-    "10.255.2.0/24",
-  ]
+  type = "list"
 }
 
-variable "accessip" {
-  default = "0.0.0.0/0"
-}
+variable "accessip" {}
